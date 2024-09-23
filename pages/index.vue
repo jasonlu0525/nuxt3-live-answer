2607a747f46483aa21db6e43c8f5e1752fce4001
JasonLu <s099113302@gmail.com>
2024-09-24 00:34:30 +0800

    upload answer

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useLoading } from "vue-loading-overlay";
+
+const $loading = useLoading({
+  backgroundColor: "green",
+  loader: "dots",
+  "is-full-page": false,
+});
+
+function openLoading() {
+  const loader = $loading.show();
+  setTimeout(() => {
+    loader.hide();
+  }, 1000);
+}
+</script>
+
+<template>
+  <button type="button" @click="openLoading">開啟 Loading 效果</button>
+</template>
+
+<style scoped></style>
