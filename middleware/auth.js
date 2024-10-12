b5942c511eb5928234aff3448a97d35dff35e4f1
JasonLu <s099113302@gmail.com>
2024-10-12 21:17:29 +0800

    clear console

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,5 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const token = useCookie("accountToken");
-  // console.log(token);
 
   if (!token.value) {
     return navigateTo("/login");
@@ -12,7 +11,6 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
       Authorization: token.value,
     },
   });
-  console.log(data, errors);
 
   if (errors?.value) {
     alert("請先登入");
